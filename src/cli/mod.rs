a4d8e150da4db94a75c01ab38c6c710815680666
Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>
2024-05-04 14:34:15 +0530

    :art: Fix code style issues
    Signed-off-by: Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,26 +1,26 @@
 #[derive(clap::Parser)]
-#[clap(version,about)]
+#[clap(version, about)]
 pub struct Command {
     #[command(subcommand)]
-    pub subcmd: SubCommand
+    pub subcmd: SubCommand,
 }
 
 #[derive(clap::Subcommand)]
 pub enum SubCommand {
     #[command(subcommand, aliases=["gen"])]
-    Generate(GenerateCmd)
+    Generate(GenerateCmd),
 }
 
 #[derive(clap::Subcommand)]
 pub enum GenerateCmd {
     Text {
-        #[clap(long,short,default_value = ".")]
+        #[clap(long, short, default_value = ".")]
         path: String,
         #[clap(long, default_value_t = false)]
         to_stdout: bool,
         #[clap(long, default_value_t = false)]
         to_stderr: bool,
-        #[clap(long,short,default_value_t = 1024)]
-        size: u128
-    }
+        #[clap(long, short, default_value_t = 1024)]
+        size: u128,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,12 @@ fn main() {
     let cmd = cli::Command::parse();
     match cmd.subcmd {
         cli::SubCommand::Generate(gen_cmd) => match gen_cmd {
-            cli::GenerateCmd::Text { path, size , to_stdout, to_stderr } => {
+            cli::GenerateCmd::Text {
+                path,
+                size,
+                to_stdout,
+                to_stderr,
+            } => {
                 let text_gen = TextGenerator::new(size);
                 let out: Box<dyn Write> = match (to_stdout, to_stderr) {
                     (true, false) => Box::new(io::stdout()),
@@ -20,14 +25,14 @@ fn main() {
                     (false, false) => {
                         let file = format!("{}/file.txt", path);
                         Box::new(fs::File::create(file).unwrap())
-                    },
-                    (true, true) => { 
+                    }
+                    (true, true) => {
                         eprintln!("Conflicting flags specified");
                         exit(1);
-                    },
+                    }
                 };
                 text_gen.generate(out);
-            },
+            }
         },
     }
 }
