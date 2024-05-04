54c4d59cf785832ecf85fda75847dfc1bc2683fa
Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>
2024-05-04 11:12:46 +0530

    :construction: Add buffering when writing to IO
    Signed-off-by: Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>

--- src/generator/text.rs
+++ src/generator/text.rs
@@ -11,11 +11,27 @@ impl TextGenerator {
     }
 }
 
+const BUF_SIZE: u16 = 256;
+
 impl Generator for TextGenerator {
     fn generate(&self, mut out: impl io::Write) {
-        for _ in 0..self.char_count {
-            out.write(fastrand::alphanumeric().to_string().as_bytes())
-                .unwrap();
+        let mut length = self.char_count;
+        while length > 0 {
+            let string = if length < (BUF_SIZE as u128) {
+                generate_string(length as u16)
+            } else {
+                length -= BUF_SIZE as u128;
+                generate_string(BUF_SIZE)
+            };
+            out.write(string.as_bytes()).unwrap();
         }
     }
+}
+
+fn generate_string(length: u16) -> String {
+    let mut string: String = "".to_string();
+    for _ in 0..length {
+        string.push(fastrand::alphanumeric());
+    }
+    return string;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::io;
 
-use dough::generator::Generator;
 use dough::generator::text::TextGenerator;
+use dough::generator::Generator;
 
 fn main() {
-    let text_gen = TextGenerator::new(256);
+    let text_gen = TextGenerator::new(1024);
     for i in 0..5 {
         print!("Run {}:\n", i);
         text_gen.generate(io::stdout());
