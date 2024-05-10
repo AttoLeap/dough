57ae660cc635a4c6147babd7b3571cea802cc19e
Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>
2024-05-10 23:41:59 +0530

    :construction: Add cli options and png image encoding
    Signed-off-by: Prajeen Govardhanam <prajeen.govardhanam@attoleap.com>

--- src/generator/image.rs
+++ src/generator/image.rs
@@ -1,3 +1,7 @@
+
+
+use image::{codecs::png::PngEncoder, ImageBuffer, ImageEncoder, Rgb};
+
 use super::Generator;
 
 pub struct ImageGenerator {
@@ -7,28 +11,45 @@ pub struct ImageGenerator {
 
 impl ImageGenerator {
     pub fn new(width: u32, height: u32) -> ImageGenerator {
-        return ImageGenerator {width, height}
+        return ImageGenerator { width, height };
+    }
+
+    fn generate_image_buffer(&self) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+        let mut buf = image::ImageBuffer::new(self.width, self.height);
+        for (x, y, pixel) in buf.enumerate_pixels_mut() {
+            let r = (0.3 * x as f64) as u8;
+            let g = (0.4 * x.abs_diff(y) as f64) as u8;
+            let b = (0.5 * y as f64) as u8;
+            *pixel = image::Rgb([r, g, b]);
+        }
+        buf
     }
 }
 
 impl Generator for ImageGenerator {
-    fn generate(&self, mut out: impl std::io::Write) {
-        let mut buf = image::ImageBuffer::new(self.width, self.height);
-        for (x, y, pixel) in buf.enumerate_pixels_mut() {
-            let r = (0.4 * x as f64) as u8;
-            let b = (0.6 * y as f64) as u8;
-            *pixel = image::Rgb([r, 0, b]);
-        }
-        out.write(&buf.to_vec()).unwrap();
+    fn generate(&self, out: impl std::io::Write) {
+        let buf = self.generate_image_buffer();
+        let encoder = PngEncoder::new(out);
+        encoder
+            .write_image(
+                &buf.to_vec(),
+                self.width,
+                self.height,
+                image::ExtendedColorType::Rgb8,
+            )
+            .unwrap();
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
+    use image::{codecs::png::PngDecoder, ImageDecoder};
+
     use crate::generator::Generator;
 
     use super::ImageGenerator;
-
 
     #[test]
     fn test_image_size() {
@@ -36,9 +57,21 @@ mod tests {
         let height = 900;
         let result_size = (width * height * 3) as usize;
         let img_gen = ImageGenerator::new(width, height);
-        let mut buf = Vec::new();
-        img_gen.generate(&mut buf);
+        let buf = img_gen.generate_image_buffer();
         assert_eq!(buf.len(), result_size);
     }
 
+    #[test]
+    fn test_image_encoder() {
+        let width = 900;
+        let height = 500;
+        let result_size = (width * height * 3) as u64;
+        let img_gen = ImageGenerator::new(width, height);
+        let mut buf = Vec::new();
+        img_gen.generate(&mut buf);
+        let decoder = PngDecoder::new(Cursor::new(buf)).unwrap();
+        assert_eq!(decoder.total_bytes(), result_size);
+        assert_eq!(decoder.dimensions().0, width);
+        assert_eq!(decoder.dimensions().1, height);
+    }
 }
